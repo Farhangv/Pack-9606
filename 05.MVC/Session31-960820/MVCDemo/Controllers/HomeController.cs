70284084cd0b1285cc240f900eb5f035dd9daef3
Farhangv <farhangv@live.com>
2017-11-11 13:39:56 +0330

    960820

---
+++ 05.MVC/Session31-960820/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //return Content("Hello MVC");
+            return new ContentResult() {
+                Content = "Hello From Content Object",
+                ContentEncoding = Encoding.UTF8,
+                ContentType = "text/html"
+            };
+        }
+
+        public ActionResult About()
+        {
+            ViewData["CurrentDate"] = DateTime.Now.ToLongDateString();
+            return View();
+        }
+    }
+}
