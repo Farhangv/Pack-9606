be1f7f818d495626b383c99a56a0b56a02c8d20a
Farhangv <farhangv@live.com>
2017-11-13 13:35:15 +0330

    960822

---
+++ 05.MVC/Session32-960822/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //return Content("Hello MVC");
+            return new ContentResult() {
+                Content = "Hello From Content Object",
+                ContentEncoding = Encoding.UTF8,
+                ContentType = "text/html"
+            };
+        }
+
+        public ActionResult About()
+        {
+            ViewData["CurrentDate"] = DateTime.Now.ToLongDateString();
+            return View();
+        }
+
+        public ActionResult RazorDemo()
+        {
+            return View();
+        }
+
+        public ActionResult ViewBagDemo()
+        {
+            ViewBag.CurrentUserName = "John";
+            ViewBag.CurrentUserFamily = "Doe";
+            return View();
+        }
+    }
+}
---
+++ 05.MVC/Session32-960822/MVCDemo/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Location { get; set; }
+        public string FavoriteFood { get; set; }
+        public string EyeColor { get; set; }
+        public List<string> ProgrammingLanguages { get; set; }
+    }
+}
