39d21b501766a017de5b20a49dd8388134169929
Farhangv <farhangv@live.com>
2017-11-15 13:36:25 +0330

    960824

---
+++ 05.MVC/Session33-960824/EFDbFirstDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDbFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name\nFamily\nBirth Date\nPhone\nCellPhone");
+            PhoneBookEntities context = new PhoneBookEntities();
+            context.People.Add(
+                new Person()
+                {
+                    Name = Console.ReadLine(),
+                    Family = Console.ReadLine(),
+                    BirthDate = DateTime.Parse(Console.ReadLine()),
+                    Contacts = new List<Contact>()
+                    {
+                        new Contact() { Type = "PHN", Value = Console.ReadLine() },
+                        new Contact() { Type = "CLL", Value = Console.ReadLine() }
+                    }
+                }
+                );
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ 05.MVC/Session33-960824/MVCDemo/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class UsersController : Controller
+    {
+        
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //public ActionResult RegisterProcessor(FormCollection form)
+        //public ActionResult RegisterProcessor(string username, 
+        //    string password,
+        //    int age)
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            //var username = Request.Form["username"];
+            //var password = Request.Form["password"];
+            //var username = form["username"];
+            //var password = form["password"];
+
+            //عملیات ثبت نام
+            TempData["Message"] = $"{user.Username} عزیز ثبت نام شما با موفقیت انجام شد، لطفا ایمیل خود را چک کنید";
+            TempData["Message"] += "<br/>";
+            TempData["Message"] += $"سن شما {user.Age} سال است";
+            //ارسال به صفحه پیام نهایی
+            return RedirectToAction("RegisterFinalMessage");
+        }
+
+        public ActionResult RegisterFinalMessage()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            User kiarash = new User()
+            {
+                Name = "کیارش",
+                Family = "کیارشی",
+                Location = "کرمانشاه",
+                FavoriteFood = "کچلیک",
+                EyeColor = "کرم",
+                ProgrammingLanguages = new List<string>()
+                {
+                    "C#","Java", "VB", "JavaScript", "Python", "Perl"
+                }
+            };
+            return View(kiarash);
+        }
+    }
+}
